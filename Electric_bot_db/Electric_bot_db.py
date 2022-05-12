2a286706e0a571ebac637b9b308b12218f60ba34
Slavianinko <90348562+Slavianinko@users.noreply.github.com>
2022-05-12 13:21:20 +0300

    Это пока шаблон

--- Electric_bot_db/Electric_bot_db.py
+++ Electric_bot_db/Electric_bot_db.py
@@ -1,67 +1,63 @@
-import random
 import json
-import logging
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 def bot(text):
 
-  return text
+    return text
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+# Определение обработчиков команд.
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
+    """Отправление сообщения, когда будет получена команда Старт"""
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
         reply_markup=ForceReply(selective=True),
     )
 
+
 def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
+    """Отправление сообщения, когда будет получена команда Help"""
     update.message.reply_text('Help!')
 
+
 def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    input_text = update.message.text
-    reply = bot(input_text)
-    update.message.reply_text(reply)
+    """Отвечаем на сообщение"""
+    #input_text = update.message.text
+    #reply = bot(input_text)
+    #update.message.reply_text(reply)
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
+    """Запуск бота"""
+    # Создаём Updater и передаём ему токен
     updater = Updater(bot_key['key'])
+
     # Get the dispatcher to register handlers
+    # Регистрация обработчиков
     dispatcher = updater.dispatcher
+
     # on different commands - answer in Telegram
+    # Ответы на команды
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
+
     # on non command i.e message - echo the message on Telegram
+    # ответ на не команду
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-    # Start the Bot
+
+    # Запуск бота
     updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
-# -------- Чтение данных из файла -------------------------------------------------
 
+# -------- Чтение данных из файла -------------------------------------------------
 with open('bot_key.json', 'r', encoding='utf-8') as f:
     bot_key = json.load(f)
 print(bot_key['key'])
-# with open('/content/BOT_CONFIG.json', 'w') as f:
-#    json.dump(BOT_CONFIG, f)
 
-
-# ------- Создание и тренировка модели ---------------------------------------------
-
-
-
-# -------- Запуск телеграм-бота ------------------------------------------------------
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     main()
